24067df9fbdcc5b9cac66ebef0fb32a66f4153dc
Kristin Reddington <kristinreddington@gmail.com>
2024-08-27 18:05:24 -0400

    install packages, add file, audiostream state variables, add file display component, add styling to homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
-export default function HomePage() {
+export default function HomePage(props) {
+  const { setAudioStream, setFile } = props;
+
+  const [recordingStatus, setRecordingStatus] = useState('inactive');
+  const [audioChunks, setAudioChunks] = useState([]);
+  const [duration, setDuration] = useState(0);
+
+  const mediaRecorder = useRef(null);
+
+  const mimeType = 'audio/webm';
+
+  async function startRecording() {
+    let tempStream
+
+    console.log('Start recording');
+
+    try {
+
+    } catch(err) {
+      console.log(err.message);
+      return
+    }
+  }
   return (
     <main className="flex-1 p-4 flex flex-col gap-3 text-center 
     sm:gap-4 md:gap-5 justify-center pb-20">
@@ -12,9 +34,13 @@ export default function HomePage() {
             <p className='text-blue-400'>Record</p>
             <i className="fa-solid fa-microphone"></i>
         </button>
-        <p className='text-base'>Or <label className='text-blue-400 cursor-pointer hover:text-blue-600 duration-200'>Upload <input className='hidden' type='file' accept='.mp3, .wave'></input>
+        <p className='text-base'>Or <label className='text-blue-400 cursor-pointer hover:text-blue-600 duration-200'>
+          Upload <input onChange={(e) => {
+            const tempFile = e.target.files[0];
+            setFile(tempFile);
+          }} className='hidden' type='file' accept='.mp3, .wave'></input>
         </label>a mp3 file</p>
-        <p className='italic text-slate-500'>Free now free forever</p>
+        <p className='italic text-slate-400'>Free now free forever</p>
     </main>
   )
 }
