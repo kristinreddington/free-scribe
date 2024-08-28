c9e13035120d0fdcff853cf10fb97269152569d6
Kristin Reddington <kristinreddington@gmail.com>
2024-08-28 12:24:44 -0400

    add styling to FileDisplay, Header components, add Transcribing & Information components,  fix HomePage conditional rendering logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import HomePage from "./components/HomePage";
 import Header from "./components/Header";
 import FileDisplay from "./components/FileDisplay";
+import Information from "./components/Information";
+import Transcribing from "./components/Transcribing";
 
 function App() {
   const [file, setFile] = useState(null);
   const [audioStream, setAudioStream] = useState(null);
   const isAudioAvailable = file || audioStream
+  const [output, setOutput] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   function handleAudioReset() {
     setFile(null);
     setAudioStream(null);
   }
+
+  useEffect(() => {
+    console.log(audioStream);
+  }, [audioStream]);
   return (
     <div className="flex flex-col max-w-[1000px] mx-auto w-full">
       <section className="min-h-screen flex flex-col">
-       <Header />
-      {isAudioAvailable ? (
-        <FileDisplay file={file} audioStream={setAudioStream} handleAudioReset={handleAudioReset} />
-      ) : ( <HomePage setFile={setFile} setAudioStream={setAudioStream} />)}
+        <Header />
+        {output ? (
+          <Information />
+        ) : loading ? (
+          <Transcribing />
+        ) : isAudioAvailable ? (
+          <FileDisplay file={file} audioStream={setAudioStream} handleAudioReset={handleAudioReset} />
+        ) : (
+          <HomePage setFile={setFile} setAudioStream={setAudioStream} />
+        )}
       </section>
       <h1></h1>
       <footer></footer>
